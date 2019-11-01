e2e2128d0b50fd72b404974e4447469868619931
Lucas Azael <42002693+Lucas1499@users.noreply.github.com>
2019-11-01 12:07:48 -0300

    Conclusão lab04

--- lab04.c
+++ lab04.c
@@ -5,15 +5,14 @@ char *alunoSituacao(char nome[16]) {
     float n1, n2, n3, n4, media;
     printf("NOTA 1 DE %s: \n", nome);
     scanf("%f",&n1);
-    printf("\nNOTA 2 DE %s: \n", nome);
+    printf("NOTA 2 DE %s: \n", nome);
     scanf("%f",&n2);
-    printf("\nNOTA 3 DE %s: \n", nome);
+    printf("NOTA 3 DE %s: \n", nome);
     scanf("%f",&n3);
-    printf("\nNOTA 4 DE %s: \n", nome);
+    printf("NOTA 4 DE %s: \n", nome);
     scanf("%f",&n4);
     media = (n1+n2+n3+n4)/4;
-    putchar('\n');
-    printf(">> MEDIA DE %s: %.2f", nome, media);
+    printf(">> MEDIA DE %s: %.2f\n", nome, media);
     putchar('\n');
     if(media>=7){
         return "APROVADO";
@@ -22,28 +21,26 @@ char *alunoSituacao(char nome[16]) {
     {
         return "REPROVADO";
     }
-    
+
 }
 void nomesAlunos(char n[][2][16]) {
     int i, j;
     char *nome;
-    char *situ;
-    for(i=0; i<5;i++){
+    for(i=0; i<2;i++){
         for(j=0;j<1;j++){
             printf("NOME: ");
             fgets(n[i][j],16,stdin);
             nome = n[i][j];
-            situ = *alunoSituacao(nome); //criar ponteiro para receber o retorno
-            j++;
-            setbuf(stdin,NULL);   
+            strcpy(n[i][j+1],alunoSituacao(nome));
+            setbuf(stdin,NULL);
                 }
             }
 }
 void mostrarAlunos(char n[][2][16]) {
     int i, j;
-    for(i=0; i<5;i++){
-        for(j=0;j<2;j++){
-            printf("%s %s", n[i][j],n[i][j+1]);  
+    for(i=0; i<2;i++){
+        for(j=0;j<1;j++){
+            printf("%s %s" , n[i][j], n[i][j+1]);
                 }
                 putchar('\n');
             }
